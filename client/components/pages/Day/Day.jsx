d5beabe376f90016ce1bd1fe88eba05f1b328b5f
Josephmusngi21 <josephmusngi7@gmail.com>
2025-08-10 23:15:31 -0700

    added files

---
+++ client/components/pages/Day/Day.jsx
@@ -0,0 +1,12 @@
+import {View,StyleSheet, Text} from "react-native";
+
+export default function Day() {
+    return (<View style={styles.container}>
+
+    </View>)
+
+};
+
+const styles = StyleSheet.create({
+
+});
---
+++ client/components/pages/Main/Main.jsx
@@ -0,0 +1,15 @@
+import {View, StyleSheet, Text} from "react-native";
+
+export default function Main() {
+    return (
+        <View style={styles.container}> 
+            <Text>Something</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+
+    },
+});
