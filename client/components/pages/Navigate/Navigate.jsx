93e1c3ac03aa78e37e28ac10e9726703fea7f023
Josephmusngi21 <josephmusngi7@gmail.com>
2025-08-10 23:28:32 -0700

    initiated styling

--- client/components/pages/Navigate/Navigate.jsx
+++ client/components/pages/Navigate/Navigate.jsx
@@ -1,31 +1,37 @@
-import {View,StyleSheet, Text, TouchableOpacity} from "react-native";
+import { View, StyleSheet, Text, TouchableOpacity } from "react-native";
 
-export default function Navigate({dateInt, yearInt}) {
-    return (
-        <View style={styles.container}>
-            <View style={styles.date}>
-                <Text style={styles.text}>{dateInt} | {yearInt}</Text>
-            </View>
-            <View style={styles.current}>
-                <Text>O</Text>
-            </View>
-            <View style={styles.menu}>
-                <TouchableOpacity>
-                    <Text>Search</Text>
-                </TouchableOpacity>
-                <TouchableOpacity>
-                    <Text>Menu</Text>
-                </TouchableOpacity>
-            </View>
-        </View>
-    )
-};
+export default function Navigate({ dateInt, yearInt }) {
+  return (
+    <View style={styles.container}>
+      <View style={styles.date}>
+        <Text style={styles.text}>
+          {dateInt} | {yearInt}
+        </Text>
+      </View>
+      <View style={styles.current}>
+        <Text>O</Text>
+      </View>
+      <View style={styles.menu}>
+        <TouchableOpacity>
+          <Text>Search</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text>Menu</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        height: 200,
-    },
+  container: {
+    display: "flex",
+    flexDirection: "row",
+    height: 200,
+    justifyContent: "space-between",
+  },
+  date: {},
+  current: {},
+  menu: { display: "flex", flexDirection: "row" },
+  text: {},
 });
